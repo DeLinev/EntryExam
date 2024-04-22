6dc14e923941f848bfb180aca7e115cabc3c18e6
Denys_L <ipz231_ldyu@student.ztu.edu.ua>
2024-04-22 11:55:28 +0300

    Add project files.

---
+++ HelloMVCApp/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HelloMVCApp.Models
+{
+    public class User
+    {
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required, StringLength(20)]
+        public string Name { get; set; }
+
+        public User()
+        {
+            Name = "Username";
+        }
+    }
+}
